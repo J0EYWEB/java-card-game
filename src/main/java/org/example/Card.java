0762afd51598995c63e52f567098fad64da77491
Josef Webster <joeywebster97@gmail.com>
2024-09-20 13:14:03 +0100

    Added Card and CardGame classes. Card has constructor for cards and CardGame creates a deck of cards

---
+++ src/main/java/org/example/Card.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Card {
+    private String suit;
+    private String symbol;
+    private int value;
+
+    public Card(String suit, String symbol, int value){
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "\n Card[" +
+                "suit='" + suit + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", value=" + value +
+                ']' + "\n";
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        CardGame deck = new CardGame();
+
+
+        System.out.println(deck.getDeckOfCards());
     }
 }
